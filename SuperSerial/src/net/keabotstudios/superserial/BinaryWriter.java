25382a08d0c72bb5b401949eaa20577a4695e5f7
Richard Abbott <richie0415@hotmail.com>
2016-12-27 21:04:12 -0500

    Edit!

--- SuperSerial/src/net/keabotstudios/superserial/BinaryWriter.java
+++ SuperSerial/src/net/keabotstudios/superserial/BinaryWriter.java
@@ -33,10 +33,46 @@ public class BinaryWriter {
 			buffer.add(data[i]);
 	}
 	
+	public void write(short data) {
+		byte[] bytes = new byte[SSType.SSDataType.SHORT.getSize()];
+		SSSerialization.write(bytes, 0, data);
+		write(bytes);
+	}
+	
+	public void write(char data) {
+		byte[] bytes = new byte[SSType.SSDataType.CHARACTER.getSize()];
+		SSSerialization.write(bytes, 0, data);
+		write(bytes);
+	}
+	
 	public void write(int data) {
-		byte[] intBytes = new byte[SSType.SSDataType.INTEGER.getSize()];
-		SSSerialization.write(intBytes, 0, data);
-		write(intBytes);
+		byte[] bytes = new byte[SSType.SSDataType.INTEGER.getSize()];
+		SSSerialization.write(bytes, 0, data);
+		write(bytes);
+	}
+	
+	public void write(float data) {
+		byte[] bytes = new byte[SSType.SSDataType.FLOAT.getSize()];
+		SSSerialization.write(bytes, 0, data);
+		write(bytes);
+	}
+	
+	public void write(long data) {
+		byte[] bytes = new byte[SSType.SSDataType.LONG.getSize()];
+		SSSerialization.write(bytes, 0, data);
+		write(bytes);
+	}
+	
+	public void write(double data) {
+		byte[] bytes = new byte[SSType.SSDataType.DOUBLE.getSize()];
+		SSSerialization.write(bytes, 0, data);
+		write(bytes);
+	}
+
+	public void write(boolean data) {
+		byte[] bytes = new byte[SSType.SSDataType.BOOLEAN.getSize()];
+		SSSerialization.write(bytes, 0, data);
+		write(bytes);
 	}
 
 }
